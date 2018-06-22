c9e79792369765c01a5270b36e65f1448c966f6b
Yan Wang <wangyan6783@gmail.com>
2018-06-22 13:44:41 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,9 +27,9 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
-  if songs.include?(input)
+  if input.class == String && songs.include?(input)
     puts "Playing #{input}"
-  elsif songs[input-1]
+  elsif input.class == songs[input-1]
     puts "Playing #{songs[input-1]}"
   else
     puts "Invalid input, please try again"
