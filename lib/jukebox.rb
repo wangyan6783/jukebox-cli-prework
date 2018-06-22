f007284f2323d2b6f855945a4ddd3792896b3ef1
Yan Wang <wangyan6783@gmail.com>
2018-06-22 12:55:40 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,5 +11,9 @@ songs = [
 ]
 
 def help
-  puts ""
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
 end
